fecaf88b0754b190213e5f140ae29e136a1fa84c
DacA1337 <67442322+DacA1337@users.noreply.github.com>
2021-06-12 17:05:22 +0900

    enemies

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -6,7 +6,10 @@ public class EnemySpawner : MonoBehaviour
 {
     public GameObject[] enemies;
     public Transform[] spawnPoints;
-    // Start is called before the first frame update
+
+    private float timeStart = 0;
+    public float timeLim;
+
     void Start()
     {
         Invoke("SpawnEnemyTop", 1f);
@@ -16,20 +19,28 @@ public class EnemySpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timeStart += Time.deltaTime;
+        if(timeStart > timeLim)
+        {
+            Invoke("SpawnEnemyTop", 1f);
+            Invoke("SpawnEnemyBot", 1f);
+            timeStart = 0;
+        }
     }
 
-    void SpawnEnemyTop()
+    public void SpawnEnemyTop()
     {
         int enemyIndex = Random.Range(0, enemies.Length);
         int spawnIndexT = Random.Range(0, spawnPoints.Length - 3);
+            Instantiate(enemies[enemyIndex], spawnPoints[spawnIndexT].position, Quaternion.identity, spawnPoints[spawnIndexT]);
+            transform.localPosition = Vector3.zero;     
 
-        Instantiate(enemies[enemyIndex], spawnPoints[spawnIndexT]);
     }
-    void SpawnEnemyBot()
+    public void SpawnEnemyBot()
     {
         int enemyIndex = Random.Range(0, enemies.Length);
         int spawnIndexB = Random.Range(3, spawnPoints.Length);
-        Instantiate(enemies[enemyIndex], spawnPoints[spawnIndexB]);
+            Instantiate(enemies[enemyIndex], spawnPoints[spawnIndexB].position, Quaternion.identity, spawnPoints[spawnIndexB]);
+            transform.localPosition = Vector3.zero;  
     }
 }
