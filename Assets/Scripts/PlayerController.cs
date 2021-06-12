831aa27c5b2de0c215d1686e6de156d94330748b
MatasV <31031537+MatasV@users.noreply.github.com>
2021-06-13 03:18:45 +0900

    animation implmenetation done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour
     public enum Controls {Arrows, AWSD}
 
     public Controls controls;
-    
+    public Animator Animator;
     public float movementSpeed;
     private void Update()
     {
@@ -18,19 +18,35 @@ public class PlayerController : MonoBehaviour
             case Controls.Arrows:
                 if (Input.GetKey(KeyCode.LeftArrow) && transform.localPosition.x > -0.9f)
                 {
+                    transform.localRotation = Quaternion.Euler(0, 180 ,0 );
+                    Animator.SetBool("walking", true);
                     transform.localPosition += Vector3.left * movementSpeed;
                 } else if (Input.GetKey(KeyCode.RightArrow) && transform.localPosition.x < 0.9f)
                 {
+                    transform.localRotation = Quaternion.Euler(0, 0 ,0 );
+                    Animator.SetBool("walking", true);
                     transform.localPosition += Vector3.right * movementSpeed;
+                }
+                else
+                {
+                    Animator.SetBool("walking", false);
                 }
                 break;
             case Controls.AWSD:
                 if (Input.GetKey(KeyCode.A)&& transform.localPosition.x > -0.9f)
                 {
+                    transform.localRotation = Quaternion.Euler(0, 180 ,0 );
+                    Animator.SetBool("walking", true);
                     transform.localPosition += Vector3.left * movementSpeed;
                 } else if (Input.GetKey(KeyCode.D) && transform.localPosition.x < 0.9f)
                 {
+                    transform.localRotation = Quaternion.Euler(0, 0 ,0 );
+                    Animator.SetBool("walking", true);
                     transform.localPosition += Vector3.right * movementSpeed;
+                }
+                else
+                {
+                    Animator.SetBool("walking", false);
                 }
                 break;
             default:
